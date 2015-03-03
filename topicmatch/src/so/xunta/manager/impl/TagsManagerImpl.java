e2e859b3a65a80d44ebc74d33d5687a68edefaa9
baobin.fang <614024531@qq.com>
2015-03-03 11:49:49 +0800

    新增一个方法 fang

--- topicmatch/src/so/xunta/manager/impl/TagsManagerImpl.java
+++ topicmatch/src/so/xunta/manager/impl/TagsManagerImpl.java
@@ -2,14 +2,28 @@ package so.xunta.manager.impl;
 
 import java.util.List;
 
+import org.hibernate.Session;
+
 import so.xunta.entity.Tag;
 import so.xunta.manager.TagsManager;
+import so.xunta.utils.HibernateUtils;
 
 public class TagsManagerImpl implements TagsManager{
 
 	@Override
 	public void addOneTag(Tag tag) {
 		// TODO Auto-generated method stub
+		Session session = HibernateUtils.openSession();
+		try {
+			session.beginTransaction();
+			session.save(tag);
+			session.getTransaction().commit();
+		} catch (RuntimeException e) {
+			session.getTransaction().rollback();
+			throw e;
+		} finally {
+			session.close();
+		}
 		
 	}
 
@@ -41,6 +55,12 @@ public class TagsManagerImpl implements TagsManager{
 	public boolean checkUserTagIsEmpty(long userId) {
 		// TODO Auto-generated method stub
 		return false;
+	}
+	
+	public static void main(String[] args) {
+		TagsManager tagManager=new TagsManagerImpl();
+		Tag tag=new Tag(101,"足球");
+		tagManager.addOneTag(tag);
 	}
 
 }
