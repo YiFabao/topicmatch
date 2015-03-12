5ea4214e40e9325b2d62a7876b6bc997d3d211d1
baobin.fang <614024531@qq.com>
2015-03-12 12:20:41 +0800

    text

--- topicmatch/src/so/xunta/manager/impl/TagsManagerImpl.java
+++ topicmatch/src/so/xunta/manager/impl/TagsManagerImpl.java
@@ -11,7 +11,7 @@ import so.xunta.entity.Tag;
 import so.xunta.manager.TagsManager;
 import so.xunta.utils.HibernateUtils;
 
-public class TagsManagerImpl implements TagsManager{
+public class TagsManagerImpl implements TagsManager {
 
 	@Override
 	public void addOneTag(Tag tag) {
@@ -21,34 +21,35 @@ public class TagsManagerImpl implements TagsManager{
 			session.beginTransaction();
 			session.save(tag);
 			session.getTransaction().commit();
-		}catch(ConstraintViolationException c){
+		} catch (ConstraintViolationException c) {
 			System.out.println("编辑标签数据存储时，因数据重复，触发此异常");
-		}  catch (RuntimeException e) {
+		} catch (RuntimeException e) {
 			session.getTransaction().rollback();
 			throw e;
 		} finally {
 			session.close();
 		}
-		
+
 	}
 
 	@Override
 	public void addTags(List<Tag> tagList) {
 		// TODO Auto-generated method stub
 		Session session = HibernateUtils.openSession();
-		for(Tag tag : tagList){
-			try {
-				session.beginTransaction();
-				session.save(tag);
-				session.getTransaction().commit();
-			}catch(ConstraintViolationException c){
-				System.out.println("编辑标签列表数据存储时，因数据重复，触发此异常");
-			}  catch (RuntimeException e) {
-				session.getTransaction().rollback();
-				throw e;
-			} finally {
-				session.close();
+		try {
+			for (Tag tag : tagList) {
+				try {
+					session.beginTransaction();
+					session.save(tag);
+					session.getTransaction().commit();
+				} catch (ConstraintViolationException c) {
+					System.out.println("编辑标签列表数据存储时，因数据重复，触发此异常");
+				} catch (RuntimeException e) {
+					session.getTransaction().rollback();
+				}
 			}
+		} finally {
+			session.close();
 		}
 	}
 
@@ -59,7 +60,7 @@ public class TagsManagerImpl implements TagsManager{
 		try {
 			session.beginTransaction();
 			Query query = session.createQuery("from Tag where id=?").setLong(0, tagId);
-			Tag tag=(Tag) query.uniqueResult();
+			Tag tag = (Tag) query.uniqueResult();
 			session.delete(tag);
 			session.getTransaction().commit();
 		} catch (RuntimeException e) {
@@ -74,24 +75,24 @@ public class TagsManagerImpl implements TagsManager{
 	public void deleteTags(List<Long> tagIdList) {
 		// TODO Auto-generated method stub
 		Session session = HibernateUtils.openSession();
-		try{
+		try {
 			session.beginTransaction();
 			int count = 0;
-			for(Long userId:tagIdList){
+			for (Long userId : tagIdList) {
 				Query query = session.createQuery("from Tag where id=?").setLong(0, userId);
 				Tag tag = (Tag) query.uniqueResult();
 				session.delete(tag);
-				if(++count % 20 == 0){
-					//当count是20的倍数时，刷新并清空session缓存，session有一级缓存限制，防止session内存溢出
+				if (++count % 20 == 0) {
+					// 当count是20的倍数时，刷新并清空session缓存，session有一级缓存限制，防止session内存溢出
 					session.flush();
 					session.clear();
 				}
 			}
 			session.getTransaction().commit();
-		}catch (RuntimeException e){
+		} catch (RuntimeException e) {
 			session.getTransaction().rollback();
 			throw e;
-		}finally{
+		} finally {
 			session.close();
 		}
 	}
@@ -124,7 +125,7 @@ public class TagsManagerImpl implements TagsManager{
 			query = session.createQuery("from Tag where userId=?").setLong(0, userId);
 			session.getTransaction().commit();
 			int total = query.list().size();
-			return total == 0 ? true:false;
+			return total == 0 ? true : false;
 		} catch (RuntimeException e) {
 			session.getTransaction().rollback();
 			throw e;
@@ -132,30 +133,30 @@ public class TagsManagerImpl implements TagsManager{
 			session.close();
 		}
 	}
-	
+
 	public static void main(String[] args) {
-//		TagsManager tagManager=new TagsManagerImpl();
-//		Tag tag1 = new Tag(101,"足球");
-//		tagManager.addOneTag(tag1);
-//		Tag tag2 = new Tag(102,"篮球");
-//		List<Tag> list = new ArrayList<Tag>();
-//		list.add(tag1);
-//		list.add(tag2);
-//		tagManager.addTags(list);
-//		tagManager.deleteOneTagByTagId(8);
-		
-//		List<Long> list = new ArrayList<Long>();
-//		list.add((long) 102);
-//		list.add((long) 103);
-//		
-//		tagManager.deleteTags(list);
-		
-//		List<Tag> list = tagManager.findAllTagsByUserId(101);
-//		for(Tag t:list){
-//			System.out.println(t.getTagname());
-//		}
-		
-//		System.out.println(tagManager.checkUserTagIsEmpty(101));
+		// TagsManager tagManager=new TagsManagerImpl();
+		// Tag tag1 = new Tag(101,"足球");
+		// tagManager.addOneTag(tag1);
+		// Tag tag2 = new Tag(102,"篮球");
+		// List<Tag> list = new ArrayList<Tag>();
+		// list.add(tag1);
+		// list.add(tag2);
+		// tagManager.addTags(list);
+		// tagManager.deleteOneTagByTagId(8);
+
+		// List<Long> list = new ArrayList<Long>();
+		// list.add((long) 102);
+		// list.add((long) 103);
+		//
+		// tagManager.deleteTags(list);
+
+		// List<Tag> list = tagManager.findAllTagsByUserId(101);
+		// for(Tag t:list){
+		// System.out.println(t.getTagname());
+		// }
+
+		// System.out.println(tagManager.checkUserTagIsEmpty(101));
 	}
 
 }
