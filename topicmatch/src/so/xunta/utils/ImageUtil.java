9c71ba90a8804d18f9468d01083b9e060a26b204
YiFabao <1019357922@qq.com>
2015-03-26 15:53:52 +0800

    test

--- topicmatch/src/so/xunta/utils/ImageUtil.java
+++ topicmatch/src/so/xunta/utils/ImageUtil.java
@@ -16,14 +16,15 @@ import com.sun.image.codec.jpeg.JPEGCodec;
 import com.sun.image.codec.jpeg.JPEGEncodeParam;
 import com.sun.image.codec.jpeg.JPEGImageEncoder;
 
+@SuppressWarnings("restriction")
 public class ImageUtil {
 
+	@SuppressWarnings("restriction")
 	public static void resize(File originalFile, File resizedFile,
 			int newWidth, float quality) throws IOException {
 
 		if (quality > 1) {
-			throw new IllegalArgumentException(
-					"Quality has to be between 0 and 1");
+			throw new IllegalArgumentException("Quality has to be between 0 and 1");
 		}
 
 		ImageIcon ii = new ImageIcon(originalFile.getCanonicalPath());
@@ -71,8 +72,7 @@ public class ImageUtil {
 		// Encodes image as a JPEG data stream
 		JPEGImageEncoder encoder = JPEGCodec.createJPEGEncoder(out);
 
-		JPEGEncodeParam param = encoder
-				.getDefaultJPEGEncodeParam(bufferedImage);
+		JPEGEncodeParam param = encoder.getDefaultJPEGEncodeParam(bufferedImage);
 
 		param.setQuality(quality, true);
 
@@ -80,12 +80,12 @@ public class ImageUtil {
 		encoder.encode(bufferedImage);
 	} // Example usage
 
-	public static void main(String[] args) throws IOException {
+/*	public static void main(String[] args) throws IOException {
 //		 File originalImage = new File("C:\\11.jpg");
 //		 resize(originalImage, new File("c:\\11-0.jpg"),150, 0.7f);
 //		 resize(originalImage, new File("c:\\11-1.jpg"),150, 1f);
 		 File originalImage = new File("C:\\Users\\Thinkpad\\Desktop\\aaa.jpg");
 		 resize(originalImage, new File("C:\\Users\\Thinkpad\\Desktop\\aaa.jpg"),100, 0.7f);
 		 //resize(originalImage, new File("c:\\1207-1.jpg"),150, 1f);
-	}
+	}*/
 }
