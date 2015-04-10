cd217349587de6c7e9a0943e05149df478d073b2
baobin.fang <614024531@qq.com>
2015-04-10 17:25:18 +0800

    a

--- topicmatch/src/so/xunta/entity/WeixinUserInfo.java
+++ topicmatch/src/so/xunta/entity/WeixinUserInfo.java
@@ -12,71 +12,8 @@ public class WeixinUserInfo {
 	public String description;
 	public String verified_reason;
 	public String tags;
-	public String weibo_uid;
+	public String weixin_uid;
 
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getNickname() {
-		return nickname;
-	}
-
-	public void setNickname(String nickname) {
-		this.nickname = nickname;
-	}
-
-	public String getGender() {
-		return gender;
-	}
-
-	public void setGender(String gender) {
-		this.gender = gender;
-	}
-
-	public String getLocation() {
-		return location;
-	}
-
-	public void setLocation(String location) {
-		this.location = location;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public String getVerified_reason() {
-		return verified_reason;
-	}
-
-	public void setVerified_reason(String verified_reason) {
-		this.verified_reason = verified_reason;
-	}
-
-	public String getTags() {
-		return tags;
-	}
-
-	public void setTags(String tags) {
-		this.tags = tags;
-	}
-
-	public String getWeibo_uid() {
-		return weibo_uid;
-	}
-
-	public void setWeibo_uid(String weibo_uid) {
-		this.weibo_uid = weibo_uid;
-	}
 
 	public WeixinUserInfo(String nickname, String gender, String location,
 			String description, String verified_reason, String tags,
@@ -88,7 +25,87 @@ public class WeixinUserInfo {
 		this.description = description;
 		this.verified_reason = verified_reason;
 		this.tags = tags;
-		this.weibo_uid = weibo_uid;
+		this.weixin_uid = weibo_uid;
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getNickname() {
+		return nickname;
+	}
+
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+
+	public String getGender() {
+		return gender;
+	}
+
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+
+	public String getLocation() {
+		return location;
+	}
+
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+
+	public String getDescription() {
+		return description;
+	}
+
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+
+	public String getVerified_reason() {
+		return verified_reason;
+	}
+
+
+	public void setVerified_reason(String verified_reason) {
+		this.verified_reason = verified_reason;
+	}
+
+
+	public String getTags() {
+		return tags;
+	}
+
+
+	public void setTags(String tags) {
+		this.tags = tags;
+	}
+
+
+	public String getWeixin_uid() {
+		return weixin_uid;
+	}
+
+
+	public void setWeixin_uid(String weixin_uid) {
+		this.weixin_uid = weixin_uid;
 	}
 
 	
