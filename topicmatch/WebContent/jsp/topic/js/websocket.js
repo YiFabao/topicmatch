c13576287f8068f7731e3285e4fd9027293b37d1
baobin.fang <614024531@qq.com>
2015-04-15 11:26:20 +0800

    test

--- topicmatch/WebContent/jsp/topic/js/websocket.js
+++ topicmatch/WebContent/jsp/topic/js/websocket.js
@@ -156,7 +156,9 @@ function websocketEvent(userId) {
 		}else if(status == "7"){
 			//推送离线消息到前台
 			console.log("有离线消息");
-			console.log('离线消息  : '+json.offlineMessage.topicId);
+			console.log('离线消息  : '+json.offlineMessage.size());
+			console.log('离线消息  : '+json.offlineMessage.length());
+			console.log('离线消息  : '+json.offlineMessage.length);
 		}
 	}
 }
